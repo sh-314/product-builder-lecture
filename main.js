2cefd00cf1b2fd8ae268a467c1bb5fe136c015bb
dellanelly2020 <dellanelly2020@gmail.com>
2026-04-06 03:36:21 +0000

    Enhance Main mode timer size and refine Mini mode layout/app-feel

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function toggleTheme() {
 themeToggle?.addEventListener('click', toggleTheme);
 
 miniToggle?.addEventListener('click', () => {
-    window.open(window.location.href + '?mode=mini', 'TomatoTimerMini', 'width=180,height=130,menubar=no,toolbar=no,location=no,status=no');
+    window.open(window.location.href + '?mode=mini', 'TimerMini', 'width=200,height=100,menubar=no,toolbar=no,location=no,status=no,popup=yes');
 });
 
 function updateTimerDisplay() {
